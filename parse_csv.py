2740602773dc23a0b97aefd688d00df0b57f5172
Pavel Berezan <pavelberezan1998@gmail.com>
2023-10-19 23:21:11 +0300

    initial commit develop

--- parse_csv.py
+++ parse_csv.py
@@ -1,13 +1,28 @@
 import json
 
-lines = []
-# open file "BelFactory.csv" and read line by line
-with open("BelFactory.csv", "r") as f:
-    keys = f.readline().strip().split(",") #strip() delet \n in the end str
-    res = [dict(zip(keys, line.strip().split(","))) for line in f] #split() separation string sep=","
 
-print(res) #print list of dictionaries with datas from file
+# it's func parsing .csv
+def parse_csv():
+    with open("BelFactory.csv", "r") as f:
+        keys = f.readline().strip().split(",") #strip() delet \n in the end str
+        res = [dict(zip(keys, line.strip().split(","))) for line in f] #split() separation string sep=","
+    return res
 
-#open file "BelFactory.json" and write file using the module json.dump
-with open('BelFactory.json', 'w') as file:
-    json.dump(res, file, indent=4) 
+# it's func to print result
+def show(parse=None):
+    print(f"{parse}")
+
+# it's func to save result as file .json
+def save_json(save=None):
+    with open('BelFactory.json', 'w') as file:
+        json.dump(save, file, indent=4)
+
+
+def main():
+    parse_csv_ = parse_csv()
+    show(parse=parse_csv_)
+    save_json(save=parse_csv_)
+
+if __name__=="__main__":
+    main()
+
