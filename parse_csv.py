32e1755df6c4d41e9f94d499d4b1747305c7ffd1
Pavel Berezan <pavelberezan1998@gmail.com>
2023-10-21 00:32:19 +0300

    refact script

--- parse_csv.py
+++ parse_csv.py
@@ -5,17 +5,21 @@ import json
 def parse_csv():
     # open file "BelFactory.csv" and read line by line
     with open("BelFactory.csv", "r") as f:
-        keys = f.readline().strip().split(",") #strip() delet \n in the end str
-        res = [dict(zip(keys, line.strip().split(","))) for line in f] #split() separation string sep=","
+        keys = f.readline().strip().split(",")  # strip() delet \n in the end str
+        res = [
+            dict(zip(keys, line.strip().split(","))) for line in f
+        ]  # split() separation string sep=","
     return res
+
 
 # it's func to print result
 def show(parse=None):
     print(f"{parse}")
 
+
 # it's func to save result as file .json
 def save_json(save=None):
-    with open('BelFactory.json', 'w') as file:
+    with open("BelFactory.json", "w") as file:
         json.dump(save, file, indent=4)
 
 
@@ -24,5 +28,6 @@ def main():
     show(parse=parse_csv_)
     save_json(save=parse_csv_)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
